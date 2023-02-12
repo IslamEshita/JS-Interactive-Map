052e96ef39750f37de95fc760a26341178b0823a
Eshita Islam <islam.eshita@gmail.com>
2023-02-12 12:36:30 -0800

    Added a map and user location popup

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("Interactive Map");
+
+async function getCoords() {
+    let location = await new Promise((resolve, reject) => {
+         navigator.geolocation.getCurrentPosition(resolve, reject);
+     });
+
+     return [location.coords.latitude, location.coords.longitude]   
+ }
+ 
+ async function buildMap()
+ {
+    // Create map     
+    let coords = await getCoords();                                                    
+    const myMap = L.map('map', {
+        center: coords,
+        zoom: 11,
+    });
+
+    // Add openstreetmap tiles
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        minZoom: 5,
+        maxZoom: 30,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(myMap);
+
+    // Create and add a geolocation marker:
+    const marker = L.marker(coords)
+    marker.addTo(myMap).bindPopup('<p1><b>You are here</b></p1>').openPopup()
+
+ }
+
+ document.body.onload = function() {
+    let selectElement = document.getElementById('businessType');        
+    selectElement.onchange = function() {        
+        console.log(selectElement.value);
+    }
+
+ }
+
+ buildMap();
